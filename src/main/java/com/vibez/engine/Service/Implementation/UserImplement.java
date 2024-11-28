fe8862df51ddb37a913a20992b0beb80c68639d2
Oshith Roshantha <oedirisuriya@gmail.com>
2024-11-29 02:43:59 +0530

    Add UserImplement service for user creation and authentication with password hashing

---
+++ src/main/java/com/vibez/engine/Service/Implementation/UserImplement.java
@@ -0,0 +1,29 @@
+package com.vibez.engine.Service.Implementation;
+
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vibez.engine.Model.User;
+import com.vibez.engine.Repository.UserRepo;
+
+@Service
+public class UserImplement implements UserService {
+    @Autowired
+    private UserRepo userRepo;
+
+    public User createUser(User newUser) {
+        String hashedPassword = BCrypt.hashpw(newUser.getPassword(), BCrypt.gensalt());
+        newUser.setPassword(hashedPassword);
+        return userRepo.save(newUser);
+    }
+
+    public boolean authenticateUser(String email, String password) {
+        User user = userRepo.findByEmail(email);
+        if (user == null) {
+            return false;
+        }
+        return user.getPassword().equals(password);
+    }
+    
+}
