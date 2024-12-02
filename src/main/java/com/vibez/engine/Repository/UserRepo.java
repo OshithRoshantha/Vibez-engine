54b8f5adb146af13421ef6dd3f503c1c0db0f558
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-02 10:24:06 +0530

    Add method to find User by email in UserRepo and update UserImplement to implement UserService

--- src/main/java/com/vibez/engine/Repository/UserRepo.java
+++ src/main/java/com/vibez/engine/Repository/UserRepo.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.vibez.engine.Model.User;
 
 public interface UserRepo extends MongoRepository<User, ObjectId>{
-    
+    User findByEmail(String email);
 }
--- src/main/java/com/vibez/engine/Service/Implementation/UserImplement.java
+++ src/main/java/com/vibez/engine/Service/Implementation/UserImplement.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import com.vibez.engine.Model.User;
 import com.vibez.engine.Repository.UserRepo;
+import com.vibez.engine.Service.UserService;
 
 @Service
 public class UserImplement implements UserService {
