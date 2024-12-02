7de4b9541b0d4256dbab5dd5f93bf7e0e08c85fa
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-02 10:20:58 +0530

    Add getters and setters for User model fields, including default values for profile picture and about

--- src/main/java/com/vibez/engine/Model/User.java
+++ src/main/java/com/vibez/engine/Model/User.java
@@ -21,5 +21,52 @@ public class User {
     private static final String DEFAULT_PROFILE_PICTURE = "https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=";
     private static final String DEFAULT_AB = "Hey there! I am using Vibez.";
 
-    
+    public String getUserName() {
+        return userName;
+    }   
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+    public void setProfilePicture(String profilePicture) {
+        if (profilePicture == null) {
+            this.profilePicture = DEFAULT_PROFILE_PICTURE;
+        }
+        this.profilePicture = profilePicture;
+    }
+    public String getAbout() {
+        return about;
+    }
+    public void setAbout(String about) {
+        if (about == null) {
+            this.about = DEFAULT_AB;
+        }
+        this.about = about;
+    }
+    public List<ObjectId> getFriendIds() {
+        return friendIds;
+    }
+    public void setFriendIds(List<ObjectId> friendIds) {
+        this.friendIds = friendIds;
+    }
+    public List<ObjectId> getGroupIds() {
+        return groupIds;
+    }
+    public void setGroupIds(List<ObjectId> groupIds) {
+        this.groupIds = groupIds;
+    }
 }
