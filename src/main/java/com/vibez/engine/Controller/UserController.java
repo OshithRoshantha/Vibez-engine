9b66b52c5b8c3ee38c3060c7fabcb5a77fea8c70
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-02 10:49:44 +0530

    Add UserController for user registration, authentication, and profile retrieval

---
+++ src/main/java/com/vibez/engine/Controller/UserController.java
@@ -0,0 +1,35 @@
+package com.vibez.engine.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vibez.engine.Model.User;
+import com.vibez.engine.Service.UserService;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserService userService;
+    
+    @PostMapping("/register")
+    public ResponseEntity<User> createUser(@RequestBody User newUser) {
+        User user = userService.createUser(newUser);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/auth")
+    public ResponseEntity<Boolean> authenticateUser(@RequestBody User user) {
+        boolean authenticated = userService.authenticateUser(user.getEmail(), user.getPassword());
+        return ResponseEntity.ok(authenticated);
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<User> getUserProfile(@RequestBody User user) {
+        User userProfile = userService.getUserProfile(user.getEmail());
+        return ResponseEntity.ok(userProfile);
+    }
+}
