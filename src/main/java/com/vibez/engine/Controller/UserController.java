69c066dde2857f87e80a7426db890d58a6369e2e
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-02 11:58:21 +0530

    Set default values for profile picture and about in UserController during user registration

--- src/main/java/com/vibez/engine/Controller/UserController.java
+++ src/main/java/com/vibez/engine/Controller/UserController.java
@@ -10,13 +10,24 @@ import org.springframework.web.bind.annotation.RestController;
 import com.vibez.engine.Model.User;
 import com.vibez.engine.Service.UserService;
 
+
 @RestController
 public class UserController {
+
+    private static final String DEFAULT_PROFILE_PICTURE = "https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=";
+    private static final String DEFAULT_AB = "Hey there! I am using Vibez.";
+
     @Autowired
     private UserService userService;
     
     @PostMapping("/register")
     public ResponseEntity<String> createUser(@RequestBody User newUser) {
+        if (newUser.getProfilePicture() == "" || newUser.getProfilePicture() == null) { 
+            newUser.setProfilePicture(DEFAULT_PROFILE_PICTURE);
+        }
+        if (newUser.getAbout() == "" || newUser.getAbout() == null) {
+            newUser.setAbout(DEFAULT_AB);
+        }
         userService.createUser(newUser);
         return ResponseEntity.ok("Account Created!");
     }
--- src/main/java/com/vibez/engine/Model/User.java
+++ src/main/java/com/vibez/engine/Model/User.java
@@ -17,10 +17,7 @@ public class User {
     private String about; 
     private List<ObjectId> friendIds; 
     private List<ObjectId> groupIds;
-
-    private static final String DEFAULT_PROFILE_PICTURE = "https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=";
-    private static final String DEFAULT_AB = "Hey there! I am using Vibez.";
-
+    
     public String getUserName() {
         return userName;
     }   
@@ -43,18 +40,12 @@ public class User {
         return profilePicture;
     }
     public void setProfilePicture(String profilePicture) {
-        if (profilePicture == null) {
-            this.profilePicture = DEFAULT_PROFILE_PICTURE;
-        }
         this.profilePicture = profilePicture;
     }
     public String getAbout() {
         return about;
     }
     public void setAbout(String about) {
-        if (about == null) {
-            this.about = DEFAULT_AB;
-        }
         this.about = about;
     }
     public List<ObjectId> getFriendIds() {
