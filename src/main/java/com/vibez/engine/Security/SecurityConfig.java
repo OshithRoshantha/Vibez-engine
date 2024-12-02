c1b5991406cbbb207cb2022ead42ab57423b9f3f
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-02 19:54:08 +0530

    Add CustomUserDetailsService for user authentication and remove unused import in SecurityConfig

--- src/main/java/com/vibez/engine/Security/SecurityConfig.java
+++ src/main/java/com/vibez/engine/Security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.vibez.engine.Security;
 
 import org.springframework.security.config.Customizer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
---
+++ src/main/java/com/vibez/engine/Service/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.vibez.engine.Service;
+
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import com.vibez.engine.Model.User;
+import com.vibez.engine.Repository.UserRepo;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+        public UserDetails loadUserByUsername(String email){
+        User user = userRepo.findByEmail(email);
+        return new org.springframework.security.core.userdetails.User(
+            user.getUserName(), 
+            user.getPassword(), 
+            Collections.emptyList() 
+    }
+    
+}
