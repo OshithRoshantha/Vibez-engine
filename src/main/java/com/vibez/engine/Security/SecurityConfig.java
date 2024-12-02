88421f87d145c1d53434e20ca8692f2e9bf81bc6
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-02 14:04:05 +0530

    Update JWT dependencies in pom.xml and replace JwtUtil class with SecurityConfig for improved security configuration

---
+++ src/main/java/com/vibez/engine/Security/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.vibez.engine.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
