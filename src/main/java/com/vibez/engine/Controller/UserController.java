11ec2f83450b6c804f5c56edd9f36da56fa4f623
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-12 14:10:48 +0530

    Add base request mapping for GoogleAuthController and UserController

--- src/main/java/com/vibez/engine/Controller/UserController.java
+++ src/main/java/com/vibez/engine/Controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vibez.engine.Model.User;
@@ -12,6 +13,7 @@ import com.vibez.engine.Service.UserService;
 
 
 @RestController
+@RequestMapping("/vibez")
 public class UserController {
 
     private static final String DEFAULT_PROFILE_PICTURE = "https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=";
