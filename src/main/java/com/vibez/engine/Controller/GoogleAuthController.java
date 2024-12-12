92c35940d03636533ee2533d6b1efd01fc055d8e
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-12 10:13:58 +0530

    Update GoogleAuthController to use path variable for token in authentication endpoint

--- src/main/java/com/vibez/engine/Controller/GoogleAuthController.java
+++ src/main/java/com/vibez/engine/Controller/GoogleAuthController.java
@@ -2,6 +2,7 @@ package com.vibez.engine.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +13,8 @@ public class GoogleAuthController {
     @Autowired
     private GoogleAuthService googleAuthService;
 
-    @PostMapping("/googleAuth")
-    public ResponseEntity<String> GoogleAuth(String token) {
+    @PostMapping("/googleAuth/{googleToken}")
+    public ResponseEntity<String> GoogleAuth(@PathVariable String token) {
         return ResponseEntity.ok(googleAuthService.GoogleAuth(token));
     } 
 }
