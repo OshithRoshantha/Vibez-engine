0fc4fe173fcadcc169103d7ed5be9388b41020e1
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-17 16:34:37 +0530

    Fix formatting in WebSocketController

--- src/main/java/com/vibez/engine/WebSocket/WebSocketController.java
+++ src/main/java/com/vibez/engine/WebSocket/WebSocketController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller; 
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import com.vibez.engine.Model.Friendship;
