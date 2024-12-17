e5a83939dad9bcbf4e17c8409144b6dc47a4e19d
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-17 14:23:30 +0530

    Implement WebSocket messaging features; add WebSocketController and enhance VibezHandler with messaging capabilities

--- src/main/java/com/vibez/engine/WebSocket/WebSocketConfig.java
+++ src/main/java/com/vibez/engine/WebSocket/WebSocketConfig.java
@@ -1,28 +1,33 @@
 package com.vibez.engine.WebSocket;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.HandshakeInterceptor;
 
 @Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Autowired
-    private JwtHandshakeInterceptor jwtHandshakeInterceptor;
+    private HandshakeInterceptor jwtHandshakeInterceptor;
 
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(vibezHandler(), "/vibez-websocket")
-                .setAllowedOrigins("*")
-                .addInterceptors(jwtHandshakeInterceptor);
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic", "/queue");
+        config.setApplicationDestinationPrefixes("/app");
     }
-    
-    @Bean
-    WebSocketHandler vibezHandler() {
-        return new VibezHandler();
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/vibez-websocket")
+                .setAllowedOrigins("*")  
+                .addInterceptors(jwtHandshakeInterceptor)  
+                .withSockJS();  
     }
 }
+
+
--- src/main/java/com/vibez/engine/WebSocket/WebSocketController.java
+++ src/main/java/com/vibez/engine/WebSocket/WebSocketController.java
@@ -1,12 +1,12 @@
-/*package com.vibez.engine.WebSocket;
+package com.vibez.engine.WebSocket;
 
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.socket.CloseStatus;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.WebSocketMessage;
-import org.springframework.web.socket.WebSocketSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import com.vibez.engine.Model.Friendship;
 import com.vibez.engine.Model.Message;
@@ -14,51 +14,21 @@ import com.vibez.engine.Service.FriendshipService;
 import com.vibez.engine.Service.GroupsService;
 import com.vibez.engine.Service.MessageService;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import org.bson.types.ObjectId;
-
-@Component
-public class VibezHandler implements WebSocketHandler {
+@Controller
+public class WebSocketController extends TextWebSocketHandler {
 
     @Autowired
     private SimpMessagingTemplate messagingTemplate;
-    
+
     @Autowired
     private MessageService messageService;
-    
-    @Autowired
-    private FriendshipService friendshipService;
-    
+
     @Autowired
     private GroupsService groupService;
 
-    @Override
-    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        System.out.println("Connection established");
-    }
+    @Autowired
+    private FriendshipService friendshipService;
 
-    @Override
-    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
-        System.out.println("Message received: " + message.getPayload());
-    }
-
-    @Override
-    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
-        System.out.println("Error: " + exception.getMessage());
-    }
-
-    @Override
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
-        System.out.println("Connection closed");
-    }
-
-    @Override
-    public boolean supportsPartialMessages() {
-        return false;
-    }
-    
     @MessageMapping("/sendMessage")
     @SendTo("/topic/messages")
     public Message sendMessage(Message message) {
@@ -82,12 +52,6 @@ public class VibezHandler implements WebSocketHandler {
     public void changeGroupName(ObjectId groupId, String newName) {
         groupService.changeGroupName(groupId, newName);
         messagingTemplate.convertAndSend("/topic/group/" + groupId, newName);
-    }
-
-    @MessageMapping("/changeGroupDesc")
-    public void changeGroupDesc(ObjectId groupId, String newDesc) {
-        groupService.changeGroupDescp(groupId, newDesc);
-        messagingTemplate.convertAndSend("/topic/group/" + groupId, newDesc);
     }
 
     @MessageMapping("/sendFriendRequest")
@@ -115,27 +79,4 @@ public class VibezHandler implements WebSocketHandler {
             messagingTemplate.convertAndSend("/topic/friendship/" + friendId, friendship);
         }
     }
-
-    @MessageMapping("/rejectFriendRequest")
-    public void rejectFriendRequest(ObjectId userId, ObjectId friendId) {
-        String response = friendshipService.rejectFriendRequest(userId, friendId);
-        Friendship friendship = new Friendship();
-        friendship.setUserId(userId);
-        friendship.setFriendId(friendId);
-        friendship.setStatus("REJECTED");
-        messagingTemplate.convertAndSend("/topic/friendship/" + userId, friendship);
-        messagingTemplate.convertAndSend("/topic/friendship/" + friendId, friendship);
-    }
-
-    @MessageMapping("/blockFriend")
-    public void blockFriend(ObjectId userId, ObjectId friendId) {
-        String response = friendshipService.blockFriend(userId, friendId);
-        Friendship friendship = new Friendship();
-        friendship.setUserId(userId);
-        friendship.setFriendId(friendId);
-        friendship.setStatus("BLOCKED");
-        messagingTemplate.convertAndSend("/topic/friendship/" + userId, friendship);
-        messagingTemplate.convertAndSend("/topic/friendship/" + friendId, friendship);
-    }
 }
-*/
