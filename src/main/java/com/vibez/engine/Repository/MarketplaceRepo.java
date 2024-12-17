4d8bb725ee380bb239130a06d8b4cd999b46e6d3
sahannimesh22 <sahannimesh22@gmail.com>
2024-12-17 19:39:37 +0530

    Add the implementation of marketplace service interface

--- src/main/java/com/vibez/engine/Repository/MarketplaceRepo.java
+++ src/main/java/com/vibez/engine/Repository/MarketplaceRepo.java
@@ -7,9 +7,9 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.vibez.engine.Model.Marketplace;
 import org.bson.types.ObjectId;
 
-
 public interface MarketplaceRepo extends MongoRepository <Marketplace, ObjectId> {
     List<Marketplace> findByVisibleToCommunityTrue();
     List<Marketplace> findBySellerId(ObjectId sellerId);
     List<Marketplace> findByVisibleToCommunityFalseAndSellerIdIn(List<ObjectId> friendId);
+    Marketplace findByProductId(ObjectId productId);
 }
