7c32277205c01d225251de631e019d18cdb7a8e0
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-22 14:58:19 +0530

    Remove createDirectChat endpoint from DirectChatController

--- src/main/java/com/vibez/engine/Controller/DirectChatController.java
+++ src/main/java/com/vibez/engine/Controller/DirectChatController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,12 +19,6 @@ public class DirectChatController {
     
     @Autowired
     private DirectChatService directChatService;
-
-    @PostMapping("/directChat/create/{userId1}/{userId2}") //Initiate a direct chat between two users
-    public ResponseEntity<String> createDirectChat(@RequestHeader(value = "Authorization", required = true)  String token, @PathVariable String userId1, @PathVariable String userId2) {
-        String chatId = directChatService.createDirectChat(userId1, userId2);
-        return ResponseEntity.ok(chatId);
-    }
 
     @GetMapping("/directChat/{userId}") //find all direct chats of a user
     public ResponseEntity<List<String>> getDirectChatsByUser(@RequestHeader(value = "Authorization", required = true)  String token, @PathVariable String userId){
