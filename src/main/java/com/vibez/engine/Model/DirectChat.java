98415b9ee732f3c3f2cb4077270a199d79f9919a
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-22 09:30:16 +0530

    Add DirectChat model to represent direct chat entities with members, last update, and last message

---
+++ src/main/java/com/vibez/engine/Model/DirectChat.java
@@ -0,0 +1,48 @@
+package com.vibez.engine.Model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "directChats")
+public class DirectChat {
+    @Id
+    private String chatId;
+    private List<String> memberIds;
+    private LocalDateTime lastUpdate;
+    private String lastMessage;
+
+    public String getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(String chatId) {
+        this.chatId = chatId;
+    }
+
+    public List<String> getMemberIds() {
+        return memberIds;
+    }
+
+    public void setMemberIds(List<String> memberIds) {
+        this.memberIds = memberIds;
+    }
+
+    public LocalDateTime getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(LocalDateTime lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public String getLastMessage() {
+        return lastMessage;
+    }
+
+    public void setLastMessage(String lastMessage) {
+        this.lastMessage = lastMessage;
+    }
+}
