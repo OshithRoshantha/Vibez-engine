b4404b3dcabb9cbe239e9b50210f543f534690d9
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-30 10:28:15 +0530

    Add Cross-Origin Resource Sharing (CORS) support to multiple controllers

--- src/main/java/com/vibez/engine/Controller/MarketplaceController.java
+++ src/main/java/com/vibez/engine/Controller/MarketplaceController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.vibez.engine.Model.Marketplace;
 import com.vibez.engine.Service.MarketplaceService;
 
+@CrossOrigin(origins = "*" , allowedHeaders = "*")
 @RestController
 @RequestMapping("/vibez")
 public class MarketplaceController {
--- src/main/java/com/vibez/engine/Controller/UserController.java
+++ src/main/java/com/vibez/engine/Controller/UserController.java
@@ -2,6 +2,7 @@ package com.vibez.engine.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +16,7 @@ import com.vibez.engine.Model.User;
 import com.vibez.engine.Service.JwtService;
 import com.vibez.engine.Service.UserService;
 
-
+@CrossOrigin(origins = "*" , allowedHeaders = "*")
 @RestController
 @RequestMapping("/vibez")
 public class UserController {
