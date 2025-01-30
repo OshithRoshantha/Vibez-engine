688b1d53b26962267adc806bba9df53d823de36e
Oshith Roshantha <oedirisuriya@gmail.com>
2025-01-30 20:51:13 +0530

    Increase JWT expiration time from 15 to 30 minutes

--- src/main/java/com/vibez/engine/Service/JwtService.java
+++ src/main/java/com/vibez/engine/Service/JwtService.java
@@ -26,7 +26,7 @@ public class JwtService {
             .setClaims(claims) 
             .setSubject(email) 
             .setIssuedAt(new Date(System.currentTimeMillis()))
-            .setExpiration(new Date(System.currentTimeMillis() + (1000 * 60 * 15))) 
+            .setExpiration(new Date(System.currentTimeMillis() + (1000 * 60 * 30))) 
             .signWith(getSecretKey())
             .compact();
     }
