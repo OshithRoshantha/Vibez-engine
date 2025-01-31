6103d63dd30a9bb47363eaedeeaba4f56dbe4f25
Oshith Roshantha <oedirisuriya@gmail.com>
2025-01-31 20:09:00 +0530

    Add RabbitMQProducer service for message publishing

---
+++ src/main/java/com/vibez/engine/RabbitMQ/Producer/RabbitMQProducer.java
@@ -0,0 +1,30 @@
+package com.vibez.engine.RabbitMQ.Producer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQProducer {
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    private static final Logger logger = LoggerFactory.getLogger(RabbitMQProducer.class);
+
+    private RabbitTemplate rabbitTemplate;
+
+    public RabbitMQProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void send(String message) {
+        logger.info("Sending message: " + message);
+        rabbitTemplate.convertAndSend(exchange, routingKey, message);
+    }
+}
