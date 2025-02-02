048f058adedcb661343da16b0c6c34763e8b4692
Oshith Roshantha <oedirisuriya@gmail.com>
2025-02-02 21:55:44 +0530

    Add CORS support to Flask application for cross-origin requests

--- vibez-intelligence/run.py
+++ vibez-intelligence/run.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from app import vibez_blueprint
+from flask_cors import CORS
 
 vibezApi = Flask(__name__)
+CORS(vibezApi, origins=["*"])
 
 vibezApi.register_blueprint(vibez_blueprint)
 
