7c0de5d3e50d7a44dd9f800232eb9a8713507061
madushankavindugit <madushankavindupg@gmail.com>
2025-03-10 21:03:40 +0530

    groupaction

---
+++ src/main/java/com/vibez/engine/Model/GroupActionv_2.java
@@ -0,0 +1,11 @@
+package com.vibez.engine.Model;
+
+import java.util.List;
+
+public class GroupActionv_2 {
+     private String groupId;
+    private String action;
+    private List<String> userIds;
+
+    
+}
