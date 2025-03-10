492185d8f49bb1e221579b86df75f8d8209d7873
sahannimesh22 <sahannimesh22@gmail.com>
2025-03-10 23:48:14 +0530

    Fix syntax errors

--- src/main/java/com/vibez/engine/Model/chat.java
+++ src/main/java/com/vibez/engine/Model/chat.java
@@ -1,10 +1,67 @@
 package com.vibez.engine.Model;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "directChats")
+@Document(collection = "chats")
 public class chat {
     @Id
     private String chatId;
+    private List<String> memberIds;
+    private LocalDateTime lastUpdate;
+    private String lastMessage;
+    private String lastMessageSender;
+    private List<String> messageIds;
+
+    public String getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(String chatId) {
+        this.chatId = chatId;
+    }
+
+    public List<String> getMemberIds() {
+        return memberIds;
+    }
+
+    public void setMemberIds(List<String> memberIds) {
+        this.memberIds = memberIds;
+    }
+
+    public LocalDateTime getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(LocalDateTime lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public String getLastMessage() {
+        return lastMessage;
+    }
+
+    public void setLastMessage(String lastMessage) {
+        this.lastMessage = lastMessage;
+    }
+
+    public List<String> getMessageIds() {
+        return messageIds;
+    }
+
+    public void setMessageIds(List<String> messageIds) {
+        this.messageIds = messageIds;
+    }
+
+    public String getLastMessageSender() {
+        return lastMessageSender;
+    }
+
+    public void setLastMessageSender(String lastMessageSender) {
+        this.lastMessageSender = lastMessageSender;
+    }
 }
+
