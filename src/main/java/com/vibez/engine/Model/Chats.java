a2ed68bb1bf7b4e96570b1d06ba25596fe895be7
sahannimesh22 <sahannimesh22@gmail.com>
2025-03-10 23:55:32 +0530

    Class Definition and Fields

---
+++ src/main/java/com/vibez/engine/Model/Chats.java
@@ -0,0 +1,18 @@
+package com.vibez.engine.Model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "directChats")
+public class Chats {
+    @Id
+    private String chatId;
+    private List<String> memberIds;
+    private LocalDateTime lastUpdate;
+    private String lastMessage;
+    private String lastMessageSender;
+    private List<String> messageIds;
+    private List<String> favoritedBy;
+}
