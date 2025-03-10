5af52ec3d7c8a46f2887ab387a17421b8520b8d1
sahannimesh22 <sahannimesh22@gmail.com>
2025-03-10 21:03:54 +0530

    Class Declaration

--- src/main/java/com/vibez/engine/Model/userv1.java
+++ src/main/java/com/vibez/engine/Model/userv1.java
@@ -3,3 +3,19 @@ package com.vibez.engine.Model;
 import java.util.List;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+@Document(collection = "users")
+public class userv1 {
+    @Id
+    private String userId;
+    private String userName;
+    private String email;
+    private String password;
+    private String profilePicture;
+    private String about;
+    private boolean darkMode;
+    private String publicKey;
+    private List<String> blockedUsers;
+    private List<String> directChatIds;
+    private List<String> groupIds;
+    private List<String> favoriteDirectChats;
+}
