b8bd2f2859d1f1501b78b0259df402b79eb0d282
sahannimesh22 <sahannimesh22@gmail.com>
2025-03-10 21:09:37 +0530

    add Getters and Setters

--- src/main/java/com/vibez/engine/Model/userv1.java
+++ src/main/java/com/vibez/engine/Model/userv1.java
@@ -43,3 +43,18 @@ public class userv1 {
         this.email = email;
     }
     }
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+    
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
